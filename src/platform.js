0e5522ad7ad568fbb421c868b2594dd48f35de87
piotr <19289194+lifeonmonday@users.noreply.github.com>
2026-08-19 23:46:41 +0200

    Update platform.js

--- src/platform.js
+++ src/platform.js
@@ -13,7 +13,7 @@ class SpotifySmartSpeakerPlatform {
     // Stany początkowe SmartSpeaker
     this.currentMediaState = this.Characteristic.CurrentMediaState.PAUSE;
     this.targetMediaState = this.Characteristic.TargetMediaState.PAUSE;
-    this.currentVolume = 50;
+    this.currentVolume = 30;
 
     this.api.on('didFinishLaunching', async () => {
       try {
