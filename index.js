51ca8581c25a7c4ac6778ac9a5d2c15d652fcd15
piotr <19289194+lifeonmonday@users.noreply.github.com>
2026-08-19 22:48:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const SpotifySmartSpeakerPlatform = require('./src/platform');
+const SpotifyTVPlatform = require('./src/tvPlatform');
+const SpotifyLightbulbPlatform = require('./src/lightbulbPlatform');
 
 module.exports = (api) => {
-  api.registerPlatform(
-    'homebridge-spotify-smart-speaker',
-    'SpotifySmartSpeaker',
-    SpotifySmartSpeakerPlatform
-  );
+  // Original Smart Speaker
+  api.registerPlatform('homebridge-spotify-smart-speaker', 'SpotifySmartSpeaker', SpotifySmartSpeakerPlatform);
+  
+  // Test TV Device (Control Center Remote)
+  api.registerPlatform('homebridge-spotify-smart-speaker', 'SpotifyTVPlatform', SpotifyTVPlatform);
+
+  // Test Lightbulb Device (Tile Tap & Volume Slider)
+  api.registerPlatform('homebridge-spotify-smart-speaker', 'SpotifyLightbulbPlatform', SpotifyLightbulbPlatform);
 };
