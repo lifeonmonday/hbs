b998ca866f01c92d583f561c69dd0634af556d54
piotr <19289194+lifeonmonday@users.noreply.github.com>
2026-08-26 19:48:46 +0200

    Update lightbulbPlatform.js
    Fix

--- src/lightbulbPlatform.js
+++ src/lightbulbPlatform.js
@@ -1,4 +1,5 @@
 const SpotifyClient = require('./spotify');
+const TriggerClient = require('./trigger'); 
 
 class SpotifyLightbulbPlatform {
   constructor(log, config, api) {
@@ -9,6 +10,7 @@ class SpotifyLightbulbPlatform {
     this.Characteristic = api.hap.Characteristic;
 
     this.client = new SpotifyClient(config, log);
+    this.triggerClient = new TriggerClient(config, log); 
     this.isPlaying = false;
     this.currentVolume = 30;
 
@@ -23,7 +25,6 @@ class SpotifyLightbulbPlatform {
   }
 
   registerAccessory() {
-    // Usunięto nawiasy ()
     const name = (this.config.name || 'Spotify Speaker') + ' Light';
     const uuid = this.api.hap.uuid.generate((this.config.deviceId || 'spotify-speaker') + '-light-test');
     const accessory = new this.api.platformAccessory(name, uuid);
@@ -36,58 +37,61 @@ class SpotifyLightbulbPlatform {
 
     this.service = accessory.addService(this.Service.Lightbulb, accessory.displayName);
 
+    // BLACH/ON CHARACTERISTIC (Włączenie/Wyłączenie)
     this.service.getCharacteristic(this.Characteristic.On)
-  .onGet(() => this.isPlaying)
-  .onSet(async (value) => {
-    if (value) {
-      try {
-        // 1. Try playing/resuming directly (fast path)
-        await this.client.play(this.config.deviceId);
-        this.isPlaying = true;
-      } catch (err) {
-        this.log.warn('Direct play failed (Nest speaker likely idle). Attempting wake-up trigger...');
+      .onGet(() => this.isPlaying)
+      .onSet(async (value) => {
+        if (value) {
+          try {
+            // 1. Próba bezpośredniego odtworzenia (szybka ścieżka)
+            await this.client.play(this.config.deviceId);
+            this.isPlaying = true;
+          } catch (err) {
+            this.log.warn('Direct play failed (Nest speaker likely idle). Attempting wake-up trigger...');
 
-        try {
-          // 2. Fire wake-up switch & wait 2s for Google/SmartThings to establish session
-          await this.triggerClient.triggerWakeupSwitch();
+            try {
+              // 2. Wyzwolenie przełącznika budzącego
+              await this.triggerClient.triggerWakeupSwitch();
 
-          // 3. Retry Spotify playback
-          await this.client.play(this.config.deviceId);
-          this.isPlaying = true;
-        } catch (retryErr) {
-          this.log.error('Playback failed even after wake-up trigger:', retryErr.message);
-          
-          // Revert HomeKit UI state back to OFF if it fails completely
-          this.isPlaying = false;
-          setTimeout(() => {
-            this.service.updateCharacteristic(this.Characteristic.On, false);
-          }, 500);
+              // 3. Ponowna próba po obudzeniu
+              await this.client.play(this.config.deviceId);
+              this.isPlaying = true;
+            } catch (retryErr) {
+              this.log.error('Playback failed even after wake-up trigger:', retryErr.message);
+
+              // Cofnięcie stanu przełącznika w Apple Home w razie porażki
+              this.isPlaying = false;
+              setTimeout(() => {
+                this.service.updateCharacteristic(this.Characteristic.On, false);
+              }, 500);
+            }
+          }
+        } else {
+          try {
+            await this.client.pause(this.config.deviceId);
+            this.isPlaying = false;
+          } catch (err) {
+            this.log.error('Błąd pauzowania Lightbulb:', err.message);
+          }
         }
-      }
-    } else {
-      try {
-        await this.client.pause(this.config.deviceId);
-        this.isPlaying = false;
-      } catch (err) {
-        this.log.error('Błąd pauzowania Lightbulb:', err.message);
-      }
-    }
-  });
-    
+      });
+
+    // BRIGHTNESS CHARACTERISTIC (Głośność ze skokiem co 5)
     this.service.getCharacteristic(this.Characteristic.Brightness)
-  .setProps({
-    minValue: 0,
-    maxValue: 80,
-    minStep: 5 
-  .onGet(() => this.currentVolume)
-  .onSet(async (value) => {
-    try {
-      await this.client.setVolume(value, this.config.deviceId);
-      this.currentVolume = value;
-    } catch (err) {
-      this.log.error('Błąd głośności Lightbulb:', err.message);
-    }
-  });
+      .setProps({
+        minValue: 0,
+        maxValue: 80,
+        minStep: 5
+      }) 
+      .onGet(() => this.currentVolume)
+      .onSet(async (value) => {
+        try {
+          await this.client.setVolume(value, this.config.deviceId);
+          this.currentVolume = value;
+        } catch (err) {
+          this.log.error('Błąd głośności Lightbulb:', err.message);
+        }
+      });
 
     this.startPolling();
     this.api.publishExternalAccessories('homebridge-spotify-smart-speaker', [accessory]);
